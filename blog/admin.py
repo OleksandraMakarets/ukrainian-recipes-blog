ce96bac280102c013ab536f5d68e1e5d31bc0ad6
Oleksandra Makarets <aleksandramakaretss@gmail.com>
2024-08-13 13:51:54 +0000

    Add JSON file to project

--- blog/admin.py
+++ blog/admin.py
@@ -6,11 +6,11 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title', 'content']
-    list_filter = ('status', 'created_on',)
+    list_display = ('title', 'slug', 'status', 'cooking_time', 'created_on')
+    search_fields = ['title', 'content', 'ingredients', 'instructions']
+    list_filter = ('status', 'created_on', 'cooking_time',)
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
+    summernote_fields = ('content', 'ingredients', 'instructions',)
 
 # Register your models here.
 admin.site.register(Comment)
