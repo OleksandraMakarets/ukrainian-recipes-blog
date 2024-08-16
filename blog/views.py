f5dbdae536dc0307c9bbf6fb8dba28b851172f60
Oleksandra Makarets <aleksandramakaretss@gmail.com>
2024-08-16 17:30:02 +0000

    Add Django messages to base.html and update post_detail view for success messages

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
+from django.contrib import messages  # Імпорт для використання повідомлень
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -8,7 +10,6 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
-
 
 def post_detail(request, slug):
     """
@@ -24,17 +25,35 @@ def post_detail(request, slug):
     :template:`blog/post_detail.html`
     """
 
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+    # Get the post object or 404 if not found
+    post = get_object_or_404(Post.objects.filter(status=1), slug=slug)
     comments = post.comments.all().order_by("-created_on")
-    comment_count = post.comments.filter(approved=True).count()  # Додаємо змінну `comment_count`
-    
+    comment_count = post.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            # Додати повідомлення про успіх
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+            # Redirect to the same post detail page to avoid resubmission on refresh
+            return redirect('post_detail', slug=post.slug)
+    else:
+        comment_form = CommentForm()
+
     return render(
         request,
-        "blog/post_detail.html",  # Перевірте, що ваш шаблон знаходиться саме в папці `blog`
+        "blog/post_detail.html",
         {
             "post": post,
             "comments": comments,
-            "comment_count": comment_count,  # Додаємо `comment_count` в контекст
+            "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
