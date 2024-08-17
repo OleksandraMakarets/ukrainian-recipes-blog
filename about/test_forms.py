3c27bb9e3ba6ad8e789cad8d2bd0d49c25a59fb4
Oleksandra Makarets <aleksandramakaretss@gmail.com>
2024-08-17 10:19:35 +0000

    Add automated tests for blog views and collaboration form

---
+++ about/test_forms.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from .forms import CollaborateForm
+
+
+class TestCollaborateForm(TestCase):
+    """Test cases for the CollaborateForm"""
+
+    def test_name_is_required(self):
+        """Test that the 'name' field is required"""
+        form = CollaborateForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Name was not provided, but the form is valid"
+        )
+
+    def test_email_is_required(self):
+        """Test that the 'email' field is required"""
+        form = CollaborateForm({
+            'name': 'Matt',
+            'email': '',
+            'message': 'Hello!'
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Email was not provided, but the form is valid"
+        )
+
+    def test_message_is_required(self):
+        """Test that the 'message' field is required"""
+        form = CollaborateForm({
+            'name': 'Matt',
+            'email': 'test@test.com',
+            'message': ''
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Message was not provided, but the form is valid"
+        )
+
